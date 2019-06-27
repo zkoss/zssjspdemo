021857587725a7a7ea73da531ebf05ffbc66d640
hawkchen <hawkchen@potix.com>
2019-06-27 17:55:58 +0800

    add more explanation in a Composer

--- src/main/java/org/zkoss/zss/jspdemo/MyComposer.java
+++ src/main/java/org/zkoss/zss/jspdemo/MyComposer.java
@@ -2,13 +2,18 @@ package org.zkoss.zss.jspdemo;
 
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zss.ui.Spreadsheet;
 
-public class MyComposer extends SelectorComposer {
+public class MyComposer extends SelectorComposer<Component> {
+
+    @Wire
+    private Spreadsheet myzss;
 
     @Override
     public void doAfterCompose(Component comp) throws Exception {
         super.doAfterCompose(comp);
-        System.out.println("initialize");
+        //initialize a Spreadsheet here e.g. load data from a database, set Spreadsheet setter, Range API
     }
 
     //implement your business logic by adding event listeners
