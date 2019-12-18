cf0aad083eef36bb259373ffb795336a4cc93ea6
hawkchen <hawkchen@potix.com>
2019-12-18 12:13:09 +0800

    add  a servlet forward example

--- src/main/java/org/zkoss/zss/jspdemo/MyComposer.java
+++ src/main/java/org/zkoss/zss/jspdemo/MyComposer.java
@@ -1,6 +1,6 @@
 package org.zkoss.zss.jspdemo;
 
-import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.*;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zss.ui.Spreadsheet;
@@ -14,6 +14,10 @@ public class MyComposer extends SelectorComposer<Component> {
     public void doAfterCompose(Component comp) throws Exception {
         super.doAfterCompose(comp);
         //initialize a Spreadsheet here e.g. load data from a database, set Spreadsheet setter, Range API
+        String file = Executions.getCurrent().getParameter("file");
+        if (file != null) {
+            myzss.setSrc("/WEB-INF/books/" + file);
+        }
     }
 
     //implement your business logic by adding event listeners
